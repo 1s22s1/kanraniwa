3ad30537f20dabecb41078952faa4b73150ca1f0
1s22s1 <1s22s1@users.noreply.github.com>
2025-02-02 12:14:00 +0900

    :up:

---
+++ 20250202/joi2025_yo1b_a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n';
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int init()
+{
+
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    return 0;
+}
+
+int main()
+{
+    init();
+
+    int T, V;
+
+    cin >> T;
+    cin >> V;
+
+    cout << T * V << endl;
+
+    return 0;
+}
